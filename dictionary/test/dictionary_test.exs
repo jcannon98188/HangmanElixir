131b5f30380adadf0c2742bac08fdd6bad476452
Jason Cannon <jcannon98188@gmail.com>
2022-10-18 19:05:45 -0700

    Adding test to dictionary module

--- dictionary/test/dictionary_test.exs
+++ dictionary/test/dictionary_test.exs
@@ -2,7 +2,8 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "greets the world" do
-    assert Dictionary.hello() == :world
+  test "Produces a word in the word list" do
+    word_list = File.read!("assets/words.txt") |> String.split(~r/\n/, trim: true)
+    assert Dictionary.random_word() in word_list
   end
 end
