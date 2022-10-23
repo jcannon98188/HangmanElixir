f61b19ab2b4a5f059c97f0e621c15fee76a114de
Jason Cannon <jcannon98188@gmail.com>
2022-10-23 13:55:07 -0700

    Refactoring Dictionary to make our public API standard

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,15 @@
 defmodule Dictionary do
-  @word_list "assets/words.txt"
-    |> File.read!()
-    |> String.split(~r/\n/, trim: true)
+  @spec start() :: list(String.t())
+  defdelegate start(), to: Dictionary.Impl.WordList
 
-  def random_word do
-    @word_list
-    |> Enum.random()
-  end
+  @spec random_word(list(String.t())) :: String.t()
+  defdelegate random_word(word_list), to: Dictionary.Impl.WordList
+  # @word_list "assets/words.txt"
+  #   |> File.read!()
+  #   |> String.split(~r/\n/, trim: true)
+
+  # def random_word do
+  #   @word_list
+  #   |> Enum.random()
+  # end
 end
