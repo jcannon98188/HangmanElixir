765f10bbee7954023799361915f6fd44feac4320
Jason Cannon <jcannon98188@gmail.com>
2022-11-09 22:56:17 -0800

    Beginning to use OTP (WIP)

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -4,12 +4,4 @@ defmodule Dictionary do
 
   @spec random_word(list(String.t())) :: String.t()
   defdelegate random_word(word_list), to: Dictionary.Impl.WordList
-  # @word_list "assets/words.txt"
-  #   |> File.read!()
-  #   |> String.split(~r/\n/, trim: true)
-
-  # def random_word do
-  #   @word_list
-  #   |> Enum.random()
-  # end
 end
