7aa137bec179ed781bae59ba79fb7f3bb2626688
Jason Cannon <jcannon98188@gmail.com>
2022-11-25 16:14:00 -0800

    adding watchdog process

--- b1/lib/b1_web/views/hangman_view.ex
+++ b1/lib/b1_web/views/hangman_view.ex
@@ -1,95 +1,32 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
 
-  def figure_for(0) do
-    ~s{
-      ┌───┐
-      │   │
-      O   │
-     /|\\  │
-     / \\  │
-          │
-    ══════╧══
-    }
+  defdelegate figure_for(turns_left), to: B1Web.HangmanView.Helpers.FigureFor
+
+  @status_fields %{
+    initializing: { "initializing", "Guess the word, a letter at a time"},
+    good_guess: {"good-guess", "Good guess!"},
+    bad_guess: {"bad-guess", "Sorry, that's a bad guess"},
+    won: {"won", "You won!"},
+    lost: {"lost", "Sorry, you lost"},
+    already_used: {"already-used", "You already used that letter"}
+  }
+  def move_status(status) do
+    {class, msg} = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>"
   end
 
-  def figure_for(1) do
-    ~s{
-      ┌───┐
-      │   │
-      O   │
-     /|\\  │
-     /    │
-          │
-    ══════╧══
-    }
+  def continue_or_try_again(conn, status) when status in [:won, :lost] do
+    button("Try again", to: Routes.hangman_path(conn, :new))
   end
 
-  def figure_for(2) do
-    ~s{
-      ┌───┐
-      │   │
-      O   │
-     /|\\  │
-          │
-          │
-    ══════╧══
-    }
-  end
-  def figure_for(3) do
-    ~s{
-      ┌───┐
-      │   │
-      O   │
-     /|   │
-          │
-          │
-    ══════╧══
-    }
-  end
-
-  def figure_for(4) do
-    ~s{
-      ┌───┐
-      │   │
-      O   │
-     /|   │
-          │
-          │
-    ══════╧══
-    }
-  end
-  def figure_for(5) do
-    ~s{
-      ┌───┐
-      │   │
-      O   │
-      |   │
-          │
-          │
-    ══════╧══
-    }
-  end
-  def figure_for(6) do
-    ~s{
-      ┌───┐
-      │   │
-      O   │
-          │
-          │
-          │
-    ══════╧══
-    }
-  end
-  def figure_for(7) do
-    ~s{
-      ┌───┐
-      │   │
-          │
-          │
-          │
-          │
-    ══════╧══
-    }
+  def continue_or_try_again(conn, _) do
+    form_for(conn, Routes.hangman_path(conn, :update), [ as: "make_move", method: :put], fn form ->
+      [
+        text_input(form, :guess),
+        " ",
+        submit("Make next guess")
+      ]
+    end)
   end
 end
--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -1,21 +1,32 @@
 defmodule Hangman.Runtime.Server do
+
+  alias Hangman.Runtime.Watchdog
+  @type t:: pid()
+  @idle_timeout 1 * 60 * 60 * 1000 # 1 hour
+
   alias Hangman.Impl.Game
   use GenServer
   @type t :: pid
-  def init(_args) do
-    {:ok, Game.new_game()}
-  end
 
+  ### client process
   def start_link(_) do
     GenServer.start_link(__MODULE__, nil)
   end
 
-  def handle_call({:make_move, guess}, _from, game) do
-    {updated_game, tally} = Game.make_move(game, guess)
-    {:reply, tally, updated_game}
+  ### server process
+  def init(_) do
+    watcher = Watchdog.start(@idle_timeout)
+    {:ok, {Game.new_game(), watcher}}
   end
 
-  def handle_call({:tally}, _from, game) do
-    {:reply, Game.tally(game), game}
+  def handle_call({:make_move, guess}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
+    {updated_game, tally} = Game.make_move(game, guess)
+    {:reply, tally, {updated_game, watcher}}
+  end
+
+  def handle_call({:tally}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
+    {:reply, Game.tally(game), {game, watcher}}
   end
 end
